a55296b6e8e88049fd6583355c8b98d929cc842d
Jack Park <jackp7640@gmail.com>
2020-03-13 21:24:25 -0700

    second commit

--- TWU_Timetable/blog/views.py
+++ TWU_Timetable/blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+	{
+		'author': 'Jack Park',
+		'title': 'Blog post 1',
+		'content': 'first post content',
+		'date_posted': 'March 13, 2020'
+	},
+	{
+		'author': 'Jack Park',
+		'title': 'Blog post 2',
+		'content': 'second post content',
+		'date_posted': 'March 14, 2020'
+	}
+]
+
+
 def home(request):
-	return render(request, 'blog/home.html')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return render(request, '')
+	return render(request, 'blog/about.html', {'title': 'about'})
 
 
