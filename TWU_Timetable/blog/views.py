15b2521d9e2e2adb078edd5de7ca82d2d370b65f
Jack Park <jackp7640@gmail.com>
2020-03-13 23:27:56 -0700

    updated database stuff

--- TWU_Timetable/blog/views.py
+++ TWU_Timetable/blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-posts = [
-	{
-		'author': 'Jack Park',
-		'title': 'Blog post 1',
-		'content': 'first post content',
-		'date_posted': 'March 13, 2020'
-	},
-	{
-		'author': 'Jack Park',
-		'title': 'Blog post 2',
-		'content': 'second post content',
-		'date_posted': 'March 14, 2020'
-	}
-]
+
 
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
