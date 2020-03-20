08140a9292d47faa293e263bb26ae08c018fabf6
Jack Park <jackp7640@gmail.com>
2020-03-19 23:42:41 -0700

    timetable size now changes based on when the day starts and ends

--- TWU_Timetable/make_table.py
+++ TWU_Timetable/make_table.py
@@ -1,8 +1,8 @@
 ##### TO DO #################################
 # 1) change colours based on course_id - FIXED
-# 2) retractable timetable size
+# 2) retractable timetable size - FIXED
 # 3) fix width of each item on the timetable - FIXED
-# 40 fix the text position
+# 40 fix the text position - FIXED
 #############################################
 
 
@@ -21,13 +21,13 @@ time_compare_list = [
                ]
 
 times = [
-             '8:00AM', '' , '', '' ,  '9:00AM', '' , '', '' ,
-            '10:00AM', '' , '', '' , '11:00AM', '' , '', '' ,
-            '12:00PM', '' , '', '' ,  '1:00PM', '' , '', '' ,
-             '2:00PM', '' , '', '' ,  '3:00PM', '' , '', '' ,
-             '4:00PM', '' , '', '' ,  '5:00PM', '' , '', '' ,
-             '6:00PM', '' , '', '' ,  '7:00PM', '' , '', '' ,
-             '8:00PM', '' , '', '' ,  '9:00PM'
+             '8:00AM', '' , '8:30AM', '' ,  '9:00AM', '' , '9:30AM', '' ,
+            '10:00AM', '' , '10:30AM', '' , '11:00AM', '' , '11:30AM', '' ,
+            '12:00PM', '' , '12:30PM', '' ,  '1:00PM', '' , '1:30PM', '' ,
+             '2:00PM', '' , '2:30PM', '' ,  '3:00PM', '' , '3:30PM', '' ,
+             '4:00PM', '' , '4:30PM', '' ,  '5:00PM', '' , '5:30PM', '' ,
+             '6:00PM', '' , '6:30PM', '' ,  '7:00PM', '' , '7:30PM', '' ,
+             '8:00PM', '' , '8:30PM', '' ,  '9:00PM'
         ]
 
 input_files=['my_data.txt']
@@ -60,9 +60,14 @@ ax.set_xticks(range(1,len(days)+1), minor=True)
 ax.set_xticklabels(days)
 
 ax.set_ylim(end_index, start_index)
-ax.set_yticks(range(0,len(times[start_index:end_index])+1))
+ax.set_yticks(range(start_index,end_index+1))
 ax.set_yticklabels(times[start_index:])
 ax.set_ylabel('Time')
+
+# ax.set_ylim(end_index, start_index)
+# ax.set_yticks(range(0,len(times[start_index:end_index])+1))
+# ax.set_yticklabels(times[start_index:])
+# ax.set_ylabel('Time')
 ### DO NOT TOUCH THESE ########################################
 
 
